ee614d7533ed6e77e8185ab05e2078b7c767d80b
vimukthi <vimukthipahasara.gg@gmail.com>
2025-05-17 16:12:04 +0000

    Finalize the auto join(hash join built-in to pandas) and custom hash join execution

--- driver.py
+++ driver.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     os.makedirs(logs_folder)
 
     engine = ExecutionEngine(csv_dataset_path, calcite_output_file, execution_tree_visualizations_folder, generated_codes_folder)
-    generated_code_paths = engine.execute_queries(join_method='auto', visualize=True, std_out_code=False)
+    generated_code_paths = engine.execute_queries(join_method='hash-join', visualize=True, std_out_code=False)
 
     for i, generated_code_path in enumerate(generated_code_paths):
         log_path = os.path.join(logs_folder, f"query_{i}.log")
