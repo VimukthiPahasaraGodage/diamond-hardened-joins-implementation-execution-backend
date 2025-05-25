d8409e03a6d5015c538b09991577e2aff1098a48
vimukthi <vimukthipahasara.gg@gmail.com>
2025-05-25 15:05:04 +0000

    Correct some issues

--- driver.py
+++ driver.py
@@ -47,9 +47,9 @@ def parse_args():
                         default='hash-join',
                         help='auto, hash-join')
 
-    parser.add_argument('--visualize', type=int, default=True, help='visualize the execution ready relational tree (0 or 1)')
-    parser.add_argument('--std_out_code', type=int, default=False, help='Whether to print the code generated for execution of the query (0 or 1)')
-    parser.add_argument('--opt', type=int, default=False, help='Optimization method to use (norma/ LE-decomposition')
+    parser.add_argument('--visualize', type=int, default=1, help='visualize the execution ready relational tree (0 or 1)')
+    parser.add_argument('--std_out_code', type=int, default=0, help='Whether to print the code generated for execution of the query (0 or 1)')
+    parser.add_argument('--opt', type=str, default='normal', help='Optimization method to use (norma/ LE-decomposition')
 
     return parser.parse_args()
 
@@ -78,10 +78,10 @@ if __name__ == '__main__':
     std_out_code = True if args.std_out_code == 1 else False
     generated_code_paths = engine.execute_queries(join_method=args.join_method, opt=args.opt, visualize=args.visualize, std_out_code=args.std_out_code)
 
-    if not generated_code_paths:
+    if generated_code_paths:
         for i, generated_code_path in enumerate(generated_code_paths):
             log_path = os.path.join(args.logs_folder, f"{Path(args.calcite_output_file).stem}.log")
-            print(f"Executing query {i} ...")
+            print(f"\nExecuting query {i} ...")
 
             with open(log_path, "w", encoding="utf-8") as log_file:
                 tee = TeeOutput(sys.stdout, log_file)
